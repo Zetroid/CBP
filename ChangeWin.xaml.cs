f0f0c84361183faad2758dc5b6ea0e995b9ee3e4
Zetroid <86872012+Zetroid@users.noreply.github.com>
2022-04-16 10:54:48 +0300

    Add files via upload

---
+++ ChangeWin.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cbp
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeWin.xaml
+    /// </summary>
+    public partial class ChangeWin : Window
+    {
+        bool Flag = false;
+        public ChangeWin()
+        {
+            InitializeComponent();
+            Comboboxcompany.ItemsSource = BaseClass.bd.Company.ToList();
+            Comboboxcompany.SelectedValuePath = "ID";
+            Comboboxcompany.DisplayMemberPath = "Title";
+
+            Comboboxtype.ItemsSource = BaseClass.bd.RequestType.ToList();
+            Comboboxtype.SelectedValuePath = "ID";
+            Comboboxtype.DisplayMemberPath = "RequestTitle";
+
+            ComboboxExecutor.ItemsSource = BaseClass.bd.Executor.ToList();
+            ComboboxExecutor.SelectedValuePath = "ID";
+            ComboboxExecutor.DisplayMemberPath = "Name";
+
+            Flag = true;
+        }
+        Request newRequest = new Request();
+        public ChangeWin(Request req)
+        {
+            InitializeComponent();
+            newRequest = req;
+
+            Comboboxcompany.ItemsSource = BaseClass.bd.Company.ToList();
+            Comboboxcompany.SelectedValuePath = "ID";
+            Comboboxcompany.DisplayMemberPath = "Title";
+
+            Comboboxtype.ItemsSource = BaseClass.bd.RequestType.ToList();
+            Comboboxtype.SelectedValuePath = "ID";
+            Comboboxtype.DisplayMemberPath = "RequestTitle";
+
+            ComboboxExecutor.ItemsSource = BaseClass.bd.Executor.ToList();
+            ComboboxExecutor.SelectedValuePath = "ID";
+            ComboboxExecutor.DisplayMemberPath = "Name";
+
+            Comboboxcompany.SelectedIndex = newRequest.CompanyID - 1;
+            Comboboxtype.SelectedIndex = newRequest.RequestTypeID - 1;
+            ComboboxExecutor.SelectedIndex = newRequest.ExecutorID - 1;
+
+            UrgencyTB.Text = newRequest.Urgency;
+            ContactTB.Text = newRequest.Contact;
+            DescriptionTB.Text = newRequest.Description;
+
+        }
+
+        private void AddBT_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                newRequest.CompanyID = Comboboxcompany.SelectedIndex + 1;
+                newRequest.RequestTypeID = Comboboxtype.SelectedIndex + 1;
+                newRequest.ExecutorID = ComboboxExecutor.SelectedIndex + 1;
+
+                newRequest.Urgency = UrgencyTB.Text;
+                newRequest.Contact = ContactTB.Text;
+                newRequest.Description = DescriptionTB.Text;
+
+                if(Flag == true)
+                {
+                    BaseClass.bd.Request.Add(newRequest);
+                }
+                BaseClass.bd.SaveChanges();
+                MessageBox.Show("Готово!");
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void DelBT_Click(object sender, RoutedEventArgs e)
+        {
+            if(Flag == false)
+            {
+                BaseClass.bd.Request.Remove(newRequest);
+                BaseClass.bd.SaveChanges();
+                MessageBox.Show("Удалено!");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Записи нет");
+            }
+        }
+    }
+}
---
+++ ListPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cbp
+{
+    /// <summary>
+    /// Логика взаимодействия для ListPage.xaml
+    /// </summary>
+    public partial class ListPage : Page
+    {
+        List<Request> Start = BaseClass.bd.Request.ToList(); 
+        List<Request> Filter;
+        public ListPage()
+        {
+            InitializeComponent();
+            Listview.ItemsSource = Start;
+            Combofilter.Items.Add("Все");
+            List<RequestType> rt = BaseClass.bd.RequestType.ToList();
+            for(int i = 0; i < rt.Count; i++)
+            {
+                Combofilter.Items.Add(rt[i].RequestTitle);
+            }
+            Combofilter.SelectedIndex = 0;
+            Combosort.SelectedIndex = 0;
+        }
+        private void FilterSort()
+        {
+            if (SearchTB.Text != String.Empty)
+            {
+                Filter = Start.Where(x => x.Description.Contains(SearchTB.Text)).ToList();
+            }
+            else
+            {
+                Filter = Start;
+            }
+
+            if(Combofilter.SelectedIndex != 0)
+            {
+                Filter = Filter.Where(x => x.RequestTypeID == Combofilter.SelectedIndex).ToList();
+            }
+
+            Listview.ItemsSource = Filter;
+            if (Combosort.SelectedIndex == 0)
+            {
+                Filter.Sort((x, y) => x.ID.CompareTo(y.ID));
+                Listview.Items.Refresh();
+            }
+            if (Combosort.SelectedIndex == 1)
+            {
+                Filter.Sort((x, y) => x.Urgency.CompareTo(y.Urgency));
+                Filter.Reverse();
+                Listview.Items.Refresh();
+            }
+            if (Combosort.SelectedIndex == 2)
+            {
+                Filter.Sort((x, y) => x.Urgency.CompareTo(y.Urgency));
+                Listview.Items.Refresh();
+            }
+            if (Combosort.SelectedIndex == 3)
+            {
+                Filter.Sort((x, y) => x.ID.CompareTo(y.ID));
+                Filter.Reverse();
+                Listview.Items.Refresh();
+            }
+        }
+        private void Combosort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterSort();
+        }
+
+        private void Combofilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FilterSort();
+        }
+
+        private void SearchTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FilterSort();
+        }
+
+        private void AddBT_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeWin win = new ChangeWin();
+            win.ShowDialog();
+            FrameClass.frame.Navigate(new ListPage());
+        }
+
+        private void ChangeBT_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            int id = Convert.ToInt32(b.Uid);
+            Request req = BaseClass.bd.Request.FirstOrDefault(x => x.ID == id);
+            ChangeWin win = new ChangeWin(req);
+            win.ShowDialog();
+            FrameClass.frame.Navigate(new ListPage());
+        }
+    }
+}
